2652cb02b4e3e14073d3c98b4b6440abdab0ad4a
Adam Rasfeld <arasfeld@gmail.com>
2026-01-31 15:39:35 -0500

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Home } from './pages/Home';
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <div>
+      <h1>💖 Hello World!</h1>
+      <p>Welcome to your Electron application.</p>
+    </div>
+  );
+}
